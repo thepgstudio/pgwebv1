7ddde1f13b4ce941aa91d13666d82f32fcf5ba2f
Areo Benjamen <areotimileyin1@gmail.com>
2024-01-09 22:35:02 +0100

    (fix): vercel analytics

--- components/layout.tsx
+++ components/layout.tsx
@@ -2,6 +2,7 @@ import Footer from "./Footer";
 import NavBar from "./NavBar";
 import React from "react";
 import ScrollToTopButton from "./TopScroller";
+import { Analytics } from "@vercel/analytics/react";
 
 type LayoutProps = {
   children: React.ReactNode;
@@ -11,7 +12,10 @@ const Layout = ({ children }: LayoutProps) => {
   return (
     <>
       <NavBar />
-      <main>{children}</main>
+      <main>
+        {children}
+        <Analytics />
+      </main>
       <ScrollToTopButton />
       <Footer />
     </>
