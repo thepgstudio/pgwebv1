f42e5801323d847139e75a6a67432453e940be00
Areo Benjamen <areotimileyin1@gmail.com>
2026-04-14 22:47:42 +0100

    refactor: remove unused theme state and web vitals reporting from App component

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,34 +1,13 @@
 import "@/styles/globals.css";
-import { useEffect, useState } from "react";
 import type { AppProps } from "next/app";
 import { ChakraProvider } from "@chakra-ui/react";
 import Layout from "../../components/layout";
-import dynamic from "next/dynamic";
 
-
-const AnimatedCursor = dynamic(() => import("react-animated-cursor"), {
-  ssr: false,
-});
-
-export function reportWebVitals(metric: any){
-  console.log(metric)
-}
 export default function App({ Component, pageProps }: AppProps) {
-  const [theme, setTheme] = useState<string>();
-
-  useEffect(() => {
-    let theme = localStorage.getItem("theme") || "light";
-    setTheme(theme);
-  }, []);
-
-  if (!theme) {
-    return;
-  }
-
   return (
     <ChakraProvider>
       <Layout>
-        <Component {...pageProps} />;
+        <Component {...pageProps} />
       </Layout>
     </ChakraProvider>
   );
